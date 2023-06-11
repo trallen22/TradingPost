dc90a6f6eb2703f483f48e49f3936f80be60b86a
trallen22 <77846813+trallen22@users.noreply.github.com>
2023-06-10 23:16:40 -0400

    completed todos 2 and 3

--- src/polygon_data.py
+++ src/polygon_data.py
@@ -69,7 +69,7 @@ def get_dataframe(curTicker, client, strToday, timeUnit, intMultiplier, printDF=
 Gets the sma values for each ticker
 '''
 # TODO4: make get_indicators script format not function 
-def get_indicators(tickers, dfPrint, dfParams, client, strToday):
+def get_indicators(tickers, dfPrint, dfParams, client, strToday, paramSet):
 
     # use '- timedelta(1) ' to debug using yesterday's data 
     today = date.today()
@@ -90,55 +90,43 @@ def get_indicators(tickers, dfPrint, dfParams, client, strToday):
     close_dict = {} # dict of closing prices
 
     for ticker in tickers:
-        
-        # TODO2: break up try statement
-        try:
+        finalIndexes = []
 
-            finalIndexes = []
-            # TODO1: Add get_dataframe parameter values to the config file
-            paramSet = [[ ticker, client, strToday, 'minute', 1, dfPrint ], 
-                            [ticker, client, strToday, "minute", 5, dfPrint], 
-                            [ticker, client, strToday, "day", 1, dfPrint]]
-
-            for i in range(len(paramSet)): 
-                curTicker = paramSet[i][0]
-                curClient = paramSet[i][1]
-                curStrToday = paramSet[i][2]
-                curTimeInterval = paramSet[i][3]
-                curMultiplier = paramSet[i][4]
-                curdfPrint = paramSet[i][5]
-
-                curDF = get_dataframe(curTicker, curClient, curStrToday, curTimeInterval, curMultiplier, curdfPrint)
+        '''
+        Generate 50 and 200 for given time interval 
+        '''
+        for i in range(len(paramSet)): 
+            # curTicker = paramSet[i][0] # need to remove 
+            curClient = paramSet[i][1]
+            curStrToday = paramSet[i][2]
+            curTimeInterval = paramSet[i][3]
+            curMultiplier = paramSet[i][4]
+            curdfPrint = paramSet[i][5]
+            try: 
+                curDF = get_dataframe(ticker, curClient, curStrToday, curTimeInterval, curMultiplier, curdfPrint)
                 finalIndexes.append(curDF[0])
                 finalIndexes.append(curDF[1])
+            except Exception as e:
+                finalIndexes.append(-1)
+                finalIndexes.append(-1) 
 
-            ticker_fifty_one_minute[ticker] = finalIndexes[0]
-            ticker_two_hundred_one_minute[ticker] = finalIndexes[1]
-            ticker_fifty_five_minute[ticker] = finalIndexes[2]
-            ticker_two_hundred_five_minute[ticker] = finalIndexes[3]
-            ticker_fifty_one_day[ticker] = finalIndexes[4]
-            ticker_two_hundred_one_day[ticker] = finalIndexes[5]
-            '''
-            Generate closing price by date 
-            '''
-            try:
-                # this needs to be run after midnight 
-                close_price = client.get_daily_open_close_agg(ticker=ticker, date=str(date.today() - timedelta(1))).close
-                close_dict[ticker] =  close_price if not close_price == '' else -1
-            except Exception as f:
-                print(f)
-                close_dict[ticker] = -1
+        ticker_fifty_one_minute[ticker] = finalIndexes[0]
+        ticker_two_hundred_one_minute[ticker] = finalIndexes[1]
+        ticker_fifty_five_minute[ticker] = finalIndexes[2]
+        ticker_two_hundred_five_minute[ticker] = finalIndexes[3]
+        ticker_fifty_one_day[ticker] = finalIndexes[4]
+        ticker_two_hundred_one_day[ticker] = finalIndexes[5]
 
-        # TODO3: make these errors on a case by case bases
-        except Exception as e:
-            print("Error from Polygon: " + str(e))
-            ticker_fifty_one_minute[ticker] = -1
-            ticker_two_hundred_one_minute[ticker] = -1
+        '''
+        Generate closing price by date 
+        '''
+        try:
+            # this needs to be run after midnight 
+            close_price = client.get_daily_open_close_agg(ticker=ticker, date=str(date.today() - timedelta(1))).close
+            close_dict[ticker] =  close_price if not close_price == '' else -1
+        except Exception as f:
+            print(f)
             close_dict[ticker] = -1
-            ticker_fifty_five_minute[ticker] = -1
-            ticker_two_hundred_five_minute[ticker] = -1
-            ticker_fifty_one_day[ticker] = -1
-            ticker_two_hundred_one_day[ticker] = -1
 
         # ensure we don't pass 5 API calls/min for polygon 
         time.sleep(30)
