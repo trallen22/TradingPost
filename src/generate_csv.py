f5577837a743722fa9bfb263516d64baed3c4106
trallen22 <77846813+trallen22@users.noreply.github.com>
2023-06-14 23:35:08 -0400

    adding progress bar for parsing through etfs

--- src/generate_csv.py
+++ src/generate_csv.py
@@ -7,26 +7,25 @@ import csv
 import time
 from polygon_data import get_indicators
 import configurationFile as config
+from tqdm import tqdm 
 
 # from email_csv import send_email
 
 def generate_csv():
 
-    TICKERS = config.TESTTICKERS
-    PARAMSET = config.PARAMSET
-
-    with open(config.TESTCSV, mode='w') as csv_file:
+    with open(config.CSVFILE, mode='a') as csv_file:
         fieldnames = ['ticker', 'one_day_50',
                     'one_day_200', 'five_min_50', 
                     'five_min_200','one_min_50', 
                     'one_min_200', 'last_price']
         writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
         writer.writeheader()
-
-        for ticker in TICKERS:
+        if (config.PBAR):
+            pBar = tqdm(desc='tickers found', total=len(config.TICKERS))
+        for ticker in config.TICKERS:
             # uses newpoly.py
             one_minute_fifty, one_minute_two_hundred, five_minute_fifty, five_minute_two_hundred, \
-        one_day_fifty, one_day_two_hundred, close_price = get_indicators(ticker, PARAMSET)
+        one_day_fifty, one_day_two_hundred, close_price = get_indicators(ticker, config.PARAMSET)
             
             writer.writerow({
                 'ticker': ticker,
@@ -38,8 +37,10 @@ def generate_csv():
                 'one_min_200': one_minute_two_hundred,
                 'last_price': close_price
             })
+            if (config.PBAR):
+                pBar.update(1)
             # ensure we don't pass 5 API calls/min for polygon 
-            if not (ticker == TICKERS[-1]):
+            if not (ticker == config.TICKERS[-1]):
                 time.sleep(60)
 
     # send_email()
