f99fb4c3511869fc66c8bc806b2d20c709a89edc
trallen22 <77846813+trallen22@users.noreply.github.com>
2023-06-18 23:21:21 -0400

    added colors for tp

--- src/configurationFile.py
+++ src/configurationFile.py
@@ -4,6 +4,7 @@ This file holds all the configuraion information
 from datetime import date
 from polygon import RESTClient 
 import os
+import openpyxl
 
 SRCPATH = os.path.dirname(__file__) + '/'
 
@@ -17,6 +18,7 @@ STRTODAY = today.strftime('%Y-%m-%d') # used with polygon data
 
 PRINTDF = 0 # prints dataframes to terminal
 PBAR = 1 # print progress bar for polygon calls in generate_csv.py 
+DEBUG = 1 # print debug messages # TODO20: add debug messages 
 
 # polygon login 
 '''Insert your key. Play around with the free tier key first.'''
@@ -30,6 +32,7 @@ PARAMSET = [[ 'minute', 1 ], # one minute time interval
 # Platform variables 
 TEMPLATEPLATFORM = SRCPATH + 'TA.WORK.xlsx'
 OUTPUTPLATFORM = SRCPATH + 'testPlatform.xlsx'
+RAWPLATFORM = SRCPATH + 'rawPlatform.xlsx' 
 
 # Trading Post variables 
 TEMPEXCEL = SRCPATH + 'stocktradingpost2.xlsx'
@@ -38,15 +41,41 @@ OUTPUTEXCEL = SRCPATH + 'testTradingPost.xlsx'
 CSVFILE = SRCPATH + 'etf.csv' 
 # CSVFILE = SRCPATH + 'etf3.csv' # used for testing 
 
-TICKERS = ["JNK", "GDX", "VCR", "VDC", "VIG", "VDE", "VFH", 
-        "VWO", "VHT", "VIS", "VGT", "VAW", "VNQ", "VOO", 
-        "VOX", "BND", "BNDX", "VXUS", "VTI", "VPU", "XTN"]
+# TICKERS = ["JNK", "GDX", "VCR", "VDC", "VIG", "VDE", "VFH", 
+#         "VWO", "VHT", "VIS", "VGT", "VAW", "VNQ", "VOO", 
+#         "VOX", "BND", "BNDX", "VXUS", "VTI", "VPU", "XTN"]
 
-# TICKERS = ["JNK"] # used for testing 
+TICKERS = ["JNK"] # used for testing 
 
 INDICATORS = ['one_day_50', 'one_day_200', 'five_min_50', 'five_min_200', 'one_min_50', 'one_min_200', 'close_price']
 
 INPUTS = { 'G':'last_price', 'H':'one_day_50', 'I':'one_day_200', 'J':'five_min_50', 
         'K':'five_min_200', 'L':'one_min_50', 'M':'one_min_200' }
+
+try: 
+        # loading excel as workbook object
+        workbook = openpyxl.load_workbook(TEMPEXCEL)
+        excelSheet = workbook.active
+
+        # Cell color templates 
+        buyRGB = excelSheet['A5'].fill.fgColor.rgb
+        BUYCOLOR = openpyxl.styles.PatternFill(start_color=buyRGB, end_color=buyRGB, fill_type='solid')
+
+        hoBuyRGB = excelSheet['C5'].fill.fgColor.rgb
+        HOBUYCOLOR = openpyxl.styles.PatternFill(start_color=hoBuyRGB, end_color=hoBuyRGB, fill_type='solid')
+
+        sellRGB = excelSheet['E5'].fill.fgColor.rgb
+        SELLCOLOR = openpyxl.styles.PatternFill(start_color=sellRGB, end_color=sellRGB, fill_type='solid')
+
+        hoSellRGB = excelSheet['F5'].fill.fgColor.rgb
+        HOSELLCOLOR = openpyxl.styles.PatternFill(start_color=hoSellRGB, end_color=hoSellRGB, fill_type='solid')
+
+        plainRGB = excelSheet['J1'].fill.fgColor.rgb
+        PLAINCOLOR = openpyxl.styles.PatternFill(start_color=plainRGB, end_color=plainRGB, fill_type='solid')
+except FileNotFoundError:
+        print(f'ERROR: File {TEMPEXCEL} could not be found')
+
+# BUYARGCOL = 'V'
+# SELLARGCOL = 'W'
 
 POSTINPUTS = {  }
