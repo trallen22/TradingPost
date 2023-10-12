5ebf01f720c425b5680db75cdcb86a00fe5a115e
trallen22 <77846813+trallen22@users.noreply.github.com>
2023-10-12 12:22:42 -0400

    small changes to sell logic

--- src/simData.py
+++ src/simData.py
@@ -9,6 +9,7 @@ import datetime
 import os
 import glob
 import openpyxl 
+from random import shuffle 
 
 AVAILABLEFUNDS = 20000
 CURRANGE = 5 # number of days/months/years
@@ -18,8 +19,8 @@ CURPORTFOLIO = {} # dictionary simulating portfolio { ticker : number of shares 
 for ticker in config.TICKERS:
     CURPORTFOLIO[ticker] = 0
 
-def totalNetworth(portfolio, availableFunds):
-    workbook = openpyxl.load_workbook(f'{config.OUTROOT}/2023-10-09_testTradingPost.xlsx')
+def totalNetworth(portfolio, availableFunds, curDay):
+    workbook = openpyxl.load_workbook(f'{config.OUTROOT}/{curDay}_testTradingPost.xlsx')
     activeSheet = workbook.active
 
     for ticker in config.TICKERS:
@@ -31,11 +32,12 @@ def totalNetworth(portfolio, availableFunds):
 
     return availableFunds
 
-def simulate(curTotalPortfolioValue, curPortfolio, initialFunds):
+def simulate(curCash, curPortfolio):
     # any file with "TradingPost" in the name 
     listFiles = glob.glob(f'{config.OUTROOT}/*TradingPost*')
+    setFiles = set(listFiles)
 
-    startDay = datetime.date.today() - datetime.timedelta(CURRANGE * TIMEUNIT)
+    startDay = datetime.date.today() - datetime.timedelta(CURRANGE * TIMEUNIT + 1)
 
     for i in range(CURRANGE * TIMEUNIT):
         curDay = startDay + datetime.timedelta(i) 
@@ -46,7 +48,7 @@ def simulate(curTotalPortfolioValue, curPortfolio, initialFunds):
             config.logmsg('DEBUG', 700, f'current day is {curDay}')
             curFile = f'{config.OUTROOT}/{curDay}_testTradingPost.xlsx'
             # check if a trading post has already been generated for curDay
-            if (curFile) in listFiles:
+            if (curFile) in setFiles:
                 config.logmsg('DEBUG', 701, f'Trading Post already created for {curDay}')
             # if trading post is not already generated for curDay 
             else:
@@ -76,43 +78,37 @@ def simulate(curTotalPortfolioValue, curPortfolio, initialFunds):
                 elif ('sell' in curSignal.lower()):
                     sellDict[ticker] = curClose
 
-            for key in buyDict:
-                numBuy = max(((initialFunds * 0.01) // buyDict[key]), 1)
-                # print(f'etf: {key}')
-                # print(f'close: {buyDict[key]}')
-                # print(f'1%: {curTotalPortfolioValue * 0.01}')
-                # print(f'num: {numBuy}')
-                # print()
+            curAccountBalance = totalNetworth(curPortfolio, curCash, curDay)
 
-                if numBuy * buyDict[key] < curTotalPortfolioValue: 
-                    curTotalPortfolioValue -= numBuy * buyDict[key]
+            buyKeys = list(buyDict.keys()) 
+            shuffle(buyKeys) # shuffling prevents always buying same etfs first 
+            for key in buyKeys:
+                numBuy = max(((curAccountBalance * 0.01) // buyDict[key]), 1)
+                if numBuy * buyDict[key] < curCash: 
+                    curCash -= numBuy * buyDict[key]
                     curPortfolio[key] += numBuy
                     config.logmsg('DEBUG', 704, f'buying {numBuy} shares of {key}')
 
             for key in sellDict:
-                # print(f'etf: {key}')
-                # print(f'close: {sellDict[key]}')
-                # print()
-
+                numSell = min(((curAccountBalance * 0.01) // sellDict[key]), curPortfolio[key])
                 if curPortfolio[key] > 0: 
-                    curTotalPortfolioValue += sellDict[key]
-                    curPortfolio[key] -= 1
-                    config.logmsg('DEBUG', 705, f'selling {key}')
+                    curCash += numSell * sellDict[key]
+                    curPortfolio[key] -= numSell
+                    config.logmsg('DEBUG', 705, f'selling {numSell} shares of {key}')
+
         else: 
             config.logmsg('DEBUG', 706, f'skipping Trading Post for {curDay} because weekday = {curDay.weekday()}')
 
-        # print('current portfolio')
-        # print(curPortfolio)
-
-    return curTotalPortfolioValue, curPortfolio 
+    return curCash, curPortfolio 
 
         
 
-funds, portfolio = simulate(AVAILABLEFUNDS, CURPORTFOLIO, AVAILABLEFUNDS)
+funds, portfolio = simulate(AVAILABLEFUNDS, CURPORTFOLIO)
 
-net = totalNetworth(portfolio, funds)
+net = totalNetworth(portfolio, funds, '2023-10-10')
 
-print(f'final net: {net}')
+print(f'initial amount: {AVAILABLEFUNDS}')
+print(f'final net: {round(net, 2)}')
 print(f'percent growth: {round(((net - AVAILABLEFUNDS) / AVAILABLEFUNDS) * 100, 2)}%')
-print(f'current cash: {funds}')
-print(f'current portfolio: {portfolio}')
+print(f'current cash: {round(funds, 2)}')
+# print(f'current portfolio: {portfolio}')
