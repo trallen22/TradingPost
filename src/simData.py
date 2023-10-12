65aeaa3e594c0779b0316f21e0fe5ef3dc77acb1
trallen22 <77846813+trallen22@users.noreply.github.com>
2023-10-12 19:07:27 -0400

    added determine sell function

--- src/simData.py
+++ src/simData.py
@@ -9,30 +9,45 @@ import datetime
 import os
 import glob
 import openpyxl 
-from random import shuffle 
+import random 
 
 AVAILABLEFUNDS = 20000
 CURRANGE = 5 # number of days/months/years
 TIMEUNIT = 365 # 365 to simulate a year 
-
-CURPORTFOLIO = {} # dictionary simulating portfolio { ticker : number of shares }
+NUMSHARESDICT = {}
+CURPORTFOLIO = {} # nested dictionary simulating portfolio { ticker : { buy price : number of shares } }
 for ticker in config.TICKERS:
-    CURPORTFOLIO[ticker] = 0
+    CURPORTFOLIO[ticker] = {}
+    NUMSHARESDICT[ticker] = 0
 
-def totalNetworth(portfolio, availableFunds, curDay):
-    workbook = openpyxl.load_workbook(f'{config.OUTROOT}/{curDay}_testTradingPost.xlsx')
-    activeSheet = workbook.active
+def determineSell(etfPortfolioDict, sellPrice):
+    buyPrice = 0
+    maxReturn = -10000000
+    for price in etfPortfolioDict:
+        curReturn = sellPrice - price
+        if curReturn > maxReturn:
+            maxReturn = curReturn 
+            buyPrice = price 
+    # returns the number of shares from best price (lot of shares) to sell 
+    return etfPortfolioDict.pop(buyPrice), etfPortfolioDict
 
-    for ticker in config.TICKERS:
+
+def totalNetworth(portfolio, availableFunds, curSheet=None):
+    if curSheet == None:
+        workbook = openpyxl.load_workbook(f'{config.OUTROOT}/{config.STRYESTERDAY}_testTradingPost.xlsx')
+        curSheet = workbook.active
+
+    # TODO: find way to only go through tickers with at least one share 
+    for ticker in list(portfolio.keys()):
         curBase = config.ETFBASECELL[ticker]
         charBase = curBase[0]
         numBase = int(curBase[1:])
-        curClose = activeSheet[f'{charBase}{numBase + 8}'].value # setting close price in tp 
+        curClose = curSheet[f'{charBase}{numBase + 8}'].value # getting close price in tp 
         availableFunds += curClose * portfolio[ticker]
 
     return availableFunds
 
-def simulate(curCash, curPortfolio):
+def simulate(curCash, curPortfolio, numSharesDict):
     # any file with "TradingPost" in the name 
     listFiles = glob.glob(f'{config.OUTROOT}/*TradingPost*')
     setFiles = set(listFiles)
@@ -45,12 +60,12 @@ def simulate(curCash, curPortfolio):
         # filter out weekends (5 is saturday and 6 is sunday) 
         if not (curDay.weekday() == 5 or curDay.weekday() == 6):
 
-            config.logmsg('DEBUG', 700, f'current day is {curDay}')
+            # config.logmsg('DEBUG', 700, f'current day is {curDay}')
             curFile = f'{config.OUTROOT}/{curDay}_testTradingPost.xlsx'
             # check if a trading post has already been generated for curDay
             if (curFile) in setFiles:
-                config.logmsg('DEBUG', 701, f'Trading Post already created for {curDay}')
-            # if trading post is not already generated for curDay 
+                pass
+#                config.logmsg('DEBUG', 701, f'Trading Post already created for {curDay}')
             else:
                 config.logmsg('NOTICE', 702, f'creating Trading Post for {curDay}')
                 try:
@@ -78,37 +93,44 @@ def simulate(curCash, curPortfolio):
                 elif ('sell' in curSignal.lower()):
                     sellDict[ticker] = curClose
 
-            curAccountBalance = totalNetworth(curPortfolio, curCash, curDay)
+            curAccountBalance = totalNetworth(numSharesDict, curCash, activeSheet)
 
             buyKeys = list(buyDict.keys()) 
-            shuffle(buyKeys) # shuffling prevents always buying same etfs first 
+            random.shuffle(buyKeys) # shuffling prevents always buying same etfs first 
             for key in buyKeys:
                 numBuy = max(((curAccountBalance * 0.01) // buyDict[key]), 1)
                 if numBuy * buyDict[key] < curCash: 
                     curCash -= numBuy * buyDict[key]
-                    curPortfolio[key] += numBuy
-                    config.logmsg('DEBUG', 704, f'buying {numBuy} shares of {key}')
+                    try: 
+                        curPortfolio[key][buyDict[key]] += numBuy # this looks like { JNK : { $20:2, $30:1 } }
+                    except Exception: 
+                        curPortfolio[key][buyDict[key]] = numBuy
+                    numSharesDict[key] += numBuy
+                    config.logmsg('DEBUG', 704, f'buying {numBuy} shares of {key} at {buyDict[key]} on {curDay}')
 
+            # sellDict is ticker and current price { ticker : current price }
             for key in sellDict:
-                numSell = min(((curAccountBalance * 0.01) // sellDict[key]), curPortfolio[key])
-                if curPortfolio[key] > 0: 
+                if (len(curPortfolio[key]) > 0): 
+                    numSell, curPortfolio[key] = determineSell(curPortfolio[key], sellDict[key])
+                    numSell = min(numSell, numSharesDict[key]) # make sure we don't sell shares we don't have. 
                     curCash += numSell * sellDict[key]
-                    curPortfolio[key] -= numSell
-                    config.logmsg('DEBUG', 705, f'selling {numSell} shares of {key}')
+                    numSharesDict[key] -= numSell
+                    config.logmsg('DEBUG', 705, f'selling {numSell} shares of {key} at {sellDict[key]} on {curDay}')
 
-        else: 
-            config.logmsg('DEBUG', 706, f'skipping Trading Post for {curDay} because weekday = {curDay.weekday()}')
+        else:
+            pass 
+#            config.logmsg('DEBUG', 706, f'skipping Trading Post for {curDay} because weekday = {curDay.weekday()}')
 
-    return curCash, curPortfolio 
+    return curCash, curPortfolio, numSharesDict
 
-        
+funds, portfolio, numShares = simulate(AVAILABLEFUNDS, CURPORTFOLIO, NUMSHARESDICT)
 
-funds, portfolio = simulate(AVAILABLEFUNDS, CURPORTFOLIO)
-
-net = totalNetworth(portfolio, funds, '2023-10-10')
+net = totalNetworth(numShares, funds)
 
 print(f'initial amount: {AVAILABLEFUNDS}')
 print(f'final net: {round(net, 2)}')
 print(f'percent growth: {round(((net - AVAILABLEFUNDS) / AVAILABLEFUNDS) * 100, 2)}%')
 print(f'current cash: {round(funds, 2)}')
-# print(f'current portfolio: {portfolio}')
+
+# for ticker in config.TICKERS:
+#     print(f'current portfolio for {ticker}:\n {portfolio[ticker]}')
