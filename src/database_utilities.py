3633950bcb10bd63f824fbdf6b72eed7b74a2f6c
trallen22 <77846813+trallen22@users.noreply.github.com>
2025-06-14 02:23:44 -0400

    adding sql management to ticker_news.py

--- src/database_utilities.py
+++ src/database_utilities.py
@@ -1,5 +1,3 @@
-from curses import curs_set
-import os
 import psycopg2
 
 # database connection variables
@@ -7,11 +5,20 @@ HOST = 'host.docker.internal' # this is for running inside the container
 USER = 'tristanallen'
 DATABASE = "trading_post"
 DB_PORT = "5432"
+# table names
+TICKER_NEWS_TABLE = "ticker_news"
+NEWS_ARTICLES_TABLE = "news_articles"
+# columns 
+ARTICLE_ID_COL = "article_id"
+ARTICLE_TITLE_COL = "article_title"
+ARTICLE_AUTHOR_COL = "article_author"
+ARTICLE_URL_COL = "article_url"
+ARTICLE_DESC_COL = "article_desc"
 # test variables
-TEST_TABLE = "related_tickers"
-TEST_TICKER = "APPL"
-TEST_RELATED = [ "GOOG", "META" ]
-TEST_DATE = "2025-06-07"
+TEST_TABLE = "ticker_news"
+TEST_TICKER = "MSFT"
+TEST_RELATED = [ "AMZN", "NVDA", "DIS" ]
+TEST_DATE = "2025-06-06"
 TEST_INSERT = (TEST_TICKER, f'{TEST_RELATED}', TEST_DATE)
 TEST_SELECT_COLS = [ "ticker_symbol", "related_tickers" ]
 
@@ -27,8 +34,8 @@ def sqlInsert(table: str, curTuple: tuple[str,str,str]) -> int:
     """
     sqlInsert: executes a SQL insert statement on a given table \\
     parameters: \\
-    	table - str, name of table to insert values into \\
-    	curTuple - tuple, tuple of values to insert into table \\
+        table - str, name of table to insert values into \\
+        curTuple - tuple, tuple of values to insert into table \\
     returns: \\
         0 - success \\
         1 - connection failed \\
@@ -40,38 +47,96 @@ def sqlInsert(table: str, curTuple: tuple[str,str,str]) -> int:
     sqlStr = f"INSERT INTO {table} VALUES ({curValStr[:-2]});"
     try:
         curCursor.execute(sqlStr, curTuple)
+        connection.commit()
     except Exception as e:
-        print(f"failed table: {table}")
-        print(f"failed insert: {curTuple}")
+        connection.rollback()
+        print(f"failed insert '{curTuple}' into '{table}'")
         print(f"ERROR: {e}")
         return 2
     # cleanup
-    connection.commit()
     curCursor.close()
     return 0
 
-def sqlSelect(table: str, columns: list[str]):
+def sqlSelect(table: str, select: list[str]=None, join: list[str]=None, where: dict=None) -> list[tuple]:
+    """
+    sqlSelect: executes a SQL select statement on a given table \\
+    parameters: \\
+        table - str, name of table to select values from \\
+        select - list, column names to select from \\
+        join - list, list of join statements \\
+        where - dict, \\
+    returns: a list of tuples replresenting the query results
+    """
     curCursor = connection.cursor()
+    # setup SELECT str
     columnStr = ""
-    for col in columns:
-        columnStr += f"{col}, "
+    if select:
+        for col in select:
+            columnStr += f"{col}, "
+        columnStr = columnStr[:-2]
+    else:
+        columnStr = "*"
+    # setup JOIN string
+    joinStr = ""
+    # TODO: Not sure is having 'join' as a list is best but it should be fine for now
+    if join:
+        joinStr = f" JOIN {join[0]} ON {join[1]}={join[2]}"
+    # setup WHERE string
+    whereStr = ""
+    if where:
+        whereStr = " WHERE "
+        for key in list(where.keys()):
+            whereStr += f"{key}='{where[key]}'"
     # running the sql statement
-    sqlStr = f"SELECT {columnStr[:-2]} FROM {table};"
+    sqlStr = f"SELECT {columnStr} FROM {table}{joinStr}{whereStr};"
+    print(f"sql statement: {sqlStr}")
     try:
         curCursor.execute(sqlStr)
+        connection.commit()
+        # fetching the results
+        queryResults = curCursor.fetchall()
     except Exception as e:
+        connection.rollback()
         print(f"failed table: {table}")
-        print(f"failed col string: {columnStr}")
         print(f"ERROR: {e}")
-        return []
-    # fetching the results
-    queryResults = curCursor.fetchall()
+        queryResults = []
     # cleanup
-    connection.commit()
     curCursor.close()
     return queryResults
 
+def sqlDelete(table: str, where: dict=None) -> int:
+    """
+    sqlDelete: executes a SQL delete statement on a given table \\
+    parameters: \\
+        table - str, name of table to select values from \\
+        where - dict, \\
+    returns: \\
+        0 - success \\
+        1 - failed to execute sql command
+    """
+    curCursor = connection.cursor()
+    # setup WHERE string
+    whereStr = ""
+    if where:
+        whereStr = " WHERE "
+        for key in list(where.keys()):
+            whereStr += f"{key}='{where[key]}'"
+    # running the sql statement
+    sqlStr = f"DELETE FROM {table}{whereStr};"
+    try:
+        curCursor.execute(sqlStr)
+        connection.commit()
+    except Exception as e:
+        connection.rollback()
+        print(f"failed table: {table}")
+        print(f"ERROR: {e}")
+        return 1
+    # cleanup
+    curCursor.close()
+    return 0
+
 if __name__ == "__main__":
     # sqlInsert(TEST_TABLE, TEST_INSERT)
-    print("test")
-    print(sqlSelect(TEST_TABLE, "*"))
+    # sqlInsert(TEST_TABLE, TEST_INSERT)
+    # print(sqlSelect(TEST_TABLE, where={"ticker_symbol": "APPL"}))
+    print(sqlDelete(TICKER_NEWS_TABLE, { "ticker_symbol": "GOOGL" }))
